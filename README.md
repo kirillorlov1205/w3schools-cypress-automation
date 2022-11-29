498deeb4c083da1e8a9cb0ce49c01eed41ce23fb
Kiryl Arlou <Kiryl.Arlou@itechart-group.com>
2022-11-29 16:20:34 +0300

    readme file has been changed

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# urban-garbanzo
-Automation framework for https://www.w3schools.com
+# w3schools-cypress-automation
+
+### w3schools-cypress-automation repository includes:
+
+#### E2E tests for https://www.w3schools.com/ web site using framework:
+
+- Cypress
+
+## Tests running:
+
+### You can use "-npm i" and run tests scripts by "npm run"
+
+**Cypress**
+
+- npm run cy-run - run Cypress tests in headed mode
+- npm run cy-open - run Cypress tests by Cypress runner app
+- npm run cy-report - generate allure report for Cypress tests
+
+### Test paths
+
+- Cypress tests - w3schools-cypress-automation/cypress
--- cypress/e2e/w3schools-sign-up.cy.ts
+++ cypress/e2e/w3schools-sign-up.cy.ts
@@ -108,20 +108,6 @@ describe('Sign up tests', () => {
         })
     }
 
-    // it.only(`Should resend verification email while clicking button "Resend email" on successful sign up page`, () => {
-    //     homePage.navigationBar.clickLoginButton()
-    //     loginPage.clickSignUpButton()
-    //     signUpPage.fillEmailField(randomEmail)
-    //     signUpPage.fillPasswordField(TEST_USER.password)
-    //     signUpPage.clickSignUpForFreeButton()
-    //     signUpPage.fillNameFieldByType(SIGN_UP_NAME_TYPES.FIRST_NAME, TEST_USER.first_name)
-    //     signUpPage.fillNameFieldByType(SIGN_UP_NAME_TYPES.LAST_NAME, TEST_USER.last_name)
-    //     signUpPage.clickSignUpForFreeButton()
-    //     signUpPage.clickResendEmailButton()
-    //     signUpPage.getValidationAlert().should('have.text', `We've resent an email to ${randomEmail} with instructions. Please check your inbox and spam folder.`)
-    //     signUpPage.getResendEmailButton().should('be.disabled')
-    // })
-
     it(`Should transfer the user to the login page while clicking button Log in on the Sign up page`, () => {
         homePage.navigationBar.clickLoginButton()
         loginPage.clickSignUpButton()
